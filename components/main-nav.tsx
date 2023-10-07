074bf3aaec9371a3909c20da07a3f0c2d019f7b5
이본훈 <80507816+LeeBonHoon1@users.noreply.github.com>
2023-10-07 16:51:30 +0900

    pay and cart

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,6 +17,7 @@ const MainNav = ({ data }: MainNavProps) => {
     label: route.name,
     active: pathname === `/category/${route.id}`,
   }));
+
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
